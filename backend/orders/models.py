114379337bf661ffe9f416f6926a7d9ce360b051
dull69 <dull69@mail.ru>
2022-06-22 00:47:34 +0300

    add Order model

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    order_num = models.PositiveIntegerField(verbose_name='Заказ №')
+    dollar_price = models.PositiveIntegerField(verbose_name='Стоимость в долларах')
+    ruble_price = models.PositiveIntegerField(verbose_name='Стоимость в рублях')
+    delivery_time = models.DateTimeField(verbose_name='Срок поставки')
+
+    def __str__(self):
+        return self.order_num
+        
