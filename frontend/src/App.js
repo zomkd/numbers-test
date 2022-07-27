0bed3ed1886496ecbb7b9b8fadd9520303ee015a
dull69 <dull69@mail.ru>
2022-07-27 23:35:27 +0300

    add websovkets on front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,90 +1,7 @@
-// import "./index.css";
-// import React from "react";
-// import {
-//   LineChart,
-//   Line,
-//   XAxis,
-//   YAxis,
-//   CartesianGrid,
-//   Tooltip,
-//   Legend
-// } from "recharts";
-
-// const data = [
-//   {
-//     name: "Page A",
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400
-//   },
-//   {
-//     name: "Page B",
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210
-//   },
-//   {
-//     name: "Page C",
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290
-//   },
-//   {
-//     name: "Page D",
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000
-//   },
-//   {
-//     name: "Page E",
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181
-//   },
-//   {
-//     name: "Page F",
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500
-//   },
-//   {
-//     name: "Page G",
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100
-//   }
-// ];
-
-// export default function App() {
-//   return (
-//     <LineChart
-//       width={500}
-//       height={300}
-//       data={data}
-//       margin={{
-//         top: 5,
-//         right: 30,
-//         left: 20,
-//         bottom: 5
-//       }}
-//     >
-//       <CartesianGrid strokeDasharray="3 3" />
-//       <XAxis dataKey="name" />
-//       <YAxis />
-//       <Tooltip />
-//       <Legend />
-//       <Line
-//         type="monotone"
-//         dataKey="pv"
-//         stroke="#8884d8"
-//         activeDot={{ r: 8 }}
-//       />
-//       <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-//     </LineChart>
-//   );
-// }
 import  React, { Component } from  'react'; 
+
 import  OrdersService  from  './OrdersService';
+import {w3cwebsocket as W3CWebSocket} from 'websocket';
 import "./index.css";
 import {
   LineChart,
@@ -122,12 +39,23 @@ class OrdersChart extends Component {
       ],
     };
   }
+  client = new W3CWebSocket('ws://127.0.0.1:8000/ws/orders/list/')
   componentDidMount() {
     var  self  =  this;
-    ordersService.getOrders().then(function (result) {
-      console.log(result);
-      self.setState({ orders:  result.data})
-    });
+    this.client.onopen = () => {
+      console.log('websucket')
+    }
+    this.client.onmessage = (gs_data) => {
+      const dataFromSever = JSON.parse(gs_data.data);
+      if (dataFromSever) {
+        console.log(dataFromSever)
+        this.setState({orders: dataFromSever})
+      }
+    }
+    // ordersService.getOrders().then(function (result) {
+    //   console.log(result);
+    //   self.setState({ orders:  result.data})
+    // });
   }
   render () {
     return (
