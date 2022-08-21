3f605f3e286eba0b8c2886bde231166caeab1893
dull69 <dull69@mail.ru>
2022-08-21 17:28:39 +0300

    delete comments

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,16 +86,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'orders_db',
-#         'USER': 'dull',
-#         'PASSWORD': 'dull',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -103,7 +93,6 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST'),
-        # 'PORT': '5432',
     }
 }
 
